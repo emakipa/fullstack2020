3809f64d122c746fa53520c4ea0bcd6a6e88b7ba
Esa Mäkipää <esa.makipaa@gmail.com>
2020-05-29 08:44:45 +0300

    osa2 exercises done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
 import personService from './services/data'
 
 const App = () => {
-  const [ persons, setPersons] = useState([]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
   const [ showAll, setShowAll ] = useState(true)
+  const [ message, setMessage ] = useState('')
+  const [ messageType, setMessageType ] = useState('')
 
   // get data from server
   useEffect(() => {
@@ -49,7 +52,12 @@ const App = () => {
             setPersons(persons.concat(returnedPerson))
             setNewName('')
             setNewNumber('')
-          })  
+          })
+        setMessage(`Added ${newName}`)
+        setMessageType('success')
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)    
     }
   }
 
@@ -61,6 +69,13 @@ const App = () => {
       personService
         .deleteSelected(id)
           .then(setPersons(persons.filter(p => p.id !== id)))
+        setMessage(`Removed information of ${deletedPerson.name}`)
+        setMessageType('success')
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+        setNewName('')
+        setNewNumber('')
     }   
   }
 
@@ -73,7 +88,24 @@ const App = () => {
         .update(id, updatedPerson)
           .then(updatedPerson => {
             setPersons(persons.map(person => person.id !== id ? person : updatedPerson))
-          })  
+            setMessage(`Updated ${updatedPerson.name}'s number`)
+            setMessageType('success')
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+            setNewName('')
+        setNewNumber('')
+          })    
+        .catch(error => {
+          setMessage(
+            `Information of ${updatedPerson.name} has already been removed from server`
+          )
+          setMessageType('error')
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+          setPersons(persons.filter(p => p.id !== id))
+        })      
   }
 
   // filter input
@@ -102,6 +134,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
+      <Notification message={message} type={messageType}/>
+
       <Filter text='filter shown with ' value={newFilter} onChange={handleFilterChange}  />
 
       <h3>Add a new</h3>
@@ -121,7 +155,7 @@ const App = () => {
       <>
         {personsToShow.map((person) => 
           <div key={person.id}>
-            <Person name={person.name} number={person.number} onClick={() => handleDeletion(person.id)} />
+            <Person name={person.name} number={person.number} onClick={() => handleDeletion(person.id)} text='delete' />
           </div>
         )}
       </>      
--- osa2/puhelinluettelo/src/components/Person.js
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Person = (props) => {
-  const label = 'delete'
-
   return (
     <div>
-      {props.name} {props.number} <button onClick={props.onClick} >{label}</button>
+      {props.name} {props.number} <button onClick={props.onClick} >{props.text}</button>
     </div>
 
   )
