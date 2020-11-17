4eab3ba88e291a5ea68d772f1f9bf49a2f8d07b1
Esa Mäkipää <esa.makipaa@gmail.com>
2020-11-17 18:36:21 +0200

    Exercise 6.1 done, reducer implemented and tested

--- osa6/unicafe-redux/src/reducer.js
+++ osa6/unicafe-redux/src/reducer.js
@@ -8,13 +8,17 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      //copy state, then change good
+      return { ...state, good: state.good + 1}
     case 'OK':
-      return state
+      //copy state, then change ok
+      return { ...state, ok: state.ok + 1}
     case 'BAD':
-      return state
+      //copy state, then change bad
+      return { ...state, bad: state.bad + 1}
     case 'ZERO':
-      return state
+      //reset
+      return { good: 0, ok: 0, bad: 0}
     default: return state
   }
   
